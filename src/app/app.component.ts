34c4eb87a84ce4f39450317dce202a9ebde463b8
Cooper <53623349+jcbreen@users.noreply.github.com>
2025-04-29 21:36:09 -0400

    adding note-pad feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NotePadComponent } from "./features/note-pad/note-pad.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NotePadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   testText = 'oops I did it again';
 }
---
+++ src/app/features/note-pad/note-pad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-pad',
+  standalone: true,
+  imports: [],
+  templateUrl: './note-pad.component.html',
+  styleUrl: './note-pad.component.scss'
+})
+export class NotePadComponent {
+
+}
