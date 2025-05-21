a64d74009958d8a1cc7673be385e25661dc905d9
Cooper <53623349+jcbreen@users.noreply.github.com>
2025-05-21 17:59:36 -0400

    adding more things :)

--- src/app/features/note-pad/note-pad.component.ts
+++ src/app/features/note-pad/note-pad.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-note-pad',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './note-pad.component.html',
-  styleUrl: './note-pad.component.scss'
+  styleUrl: './note-pad.component.scss',
 })
 export class NotePadComponent {
+  noteContent: any;
+  savedMessage: any;
 
+  saveNote() {
+    console.log('Note saved');
+  }
+  clearNote() {
+    console.log('Note cleared');
+  }
+
+  prescription() {
+    const newName: string = 'cool stuff';
+    this.noteContent;        
+  }
 }
